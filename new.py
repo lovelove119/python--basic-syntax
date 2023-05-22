f683f6d019b6d12d48271db13d9afda2af6c5a35
lovelove119 <mujung123@naver.com>
2023-05-22 17:18:50 +0900

    1일차

---
+++ new.py
@@ -0,0 +1,18 @@
+a = 3
+b = 4
+# 덧셈 : +, 빼기 - , 곱하기 *, 나누기 : / 몫 : // 나머지 : %
+print (a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+# 자료의 형변환
+# 숫자 > 문자 , 실수 > 정수
+a = 10
+b = 20
+# 결과값이 1020이 나오도록 덧셈을 하여라.
+print(str(a)+str(b))
+c = 2333.33333
+print(int(c))
